be61fee0a81bec1460c7eea6c46891fc09219a1a
LIM HEE JIN <xeexin0331@gmail.com>
2023-11-20 19:07:34 +0900

    API

--- src/main/java/spring/springintroduction/controller/HelloControler.java
+++ src/main/java/spring/springintroduction/controller/HelloControler.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloControler {
@@ -17,5 +18,31 @@ public class HelloControler {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello2";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody  // html body에 직접 넣어주겠다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
