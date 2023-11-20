1fd6fc66df383ecce9f276b512e50ba1a5b67ba0
LIM HEE JIN <xeexin0331@gmail.com>
2023-11-20 16:40:35 +0900

    환경변수 설정

---
+++ src/test/java/spring/springintroduction/SpringIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
