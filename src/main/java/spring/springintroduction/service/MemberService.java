d3a2fda0076295ef83d332576a1bca90c9df0b93
LIM HEE JIN <xeexin0331@gmail.com>
2023-11-21 12:55:07 +0900

    회원 서비스 개발

---
+++ src/main/java/spring/springintroduction/service/MemberService.java
@@ -0,0 +1,43 @@
+package spring.springintroduction.service;
+
+import spring.springintroduction.domain.Member;
+import spring.springintroduction.repository.MemberRepository;
+import spring.springintroduction.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public long join(Member member) {
+        //같은 이름 no
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
