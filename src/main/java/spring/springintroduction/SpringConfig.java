c8db5391cd28e0e72050041cc32f47efbae5d3bc
LIM HEE JIN <xeexin0331@gmail.com>
2023-11-23 21:08:28 +0900

    순수 JDBC

--- src/main/java/spring/springintroduction/SpringConfig.java
+++ src/main/java/spring/springintroduction/SpringConfig.java
@@ -1,13 +1,24 @@
 package spring.springintroduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import spring.springintroduction.repository.JdbcMemberRepository;
 import spring.springintroduction.repository.MemberRepository;
 import spring.springintroduction.repository.MemoryMemberRepository;
 import spring.springintroduction.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository() );
@@ -15,7 +26,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
